1e3df4180cff312467a8fcd7492a8ec175436ec9
MauR27 <antonio27mauricio27@gmail.com>
2023-03-02 05:26:05 +0100

    le di funcionalidad con una lib, math.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,138 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { evaluate } from "mathjs";
 
 function App() {
-  function onClick() {}
+  const [input, setInput] = useState("");
+
+  function onClickSigns(e) {
+    setInput((prev) => prev + e.target.value);
+  }
+  // console.log(input);
+
+  function result() {
+    if (input) {
+      setInput(evaluate(input));
+    }
+  }
+
+  function clear() {
+    return setInput("");
+  }
 
   // RETURN RENDER
   return (
     <div className="App">
       <div id="box-container">
-        <div id="display-box"></div>
+        <div id="display-box">
+          <h2>{input}</h2>
+          {/* <h4>{textSigns}</h4> */}
+        </div>
         <div id="buttons-box">
-          <button className="add" id="buttons">
+          <button className="add" id="buttons" onClick={onClickSigns} value="+">
             +
           </button>
-          <button className="subtract" id="buttons">
+          <button
+            className="subtract"
+            id="buttons"
+            onClick={onClickSigns}
+            value="-"
+          >
             -
           </button>
-          <button className="divide" id="buttons">
+          <button
+            className="divide"
+            id="buttons"
+            onClick={onClickSigns}
+            value="/"
+          >
             /
           </button>
-          <button className="multiply" id="buttons">
+          <button
+            className="multiply"
+            id="buttons"
+            onClick={onClickSigns}
+            value="*"
+          >
             X
           </button>
-          <button className="seven" id="buttons">
+          <button
+            className="seven"
+            id="buttons"
+            onClick={onClickSigns}
+            value="7"
+          >
             7
           </button>
-          <button className="eight" id="buttons">
+          <button
+            className="eight"
+            id="buttons"
+            onClick={onClickSigns}
+            value="8"
+          >
             8
           </button>
-          <button className="nine" id="buttons">
+          <button
+            className="nine"
+            id="buttons"
+            onClick={onClickSigns}
+            value="9"
+          >
             9
           </button>
-          <button className="four" id="buttons">
+          <button
+            className="four"
+            id="buttons"
+            onClick={onClickSigns}
+            value="4"
+          >
             4
           </button>
-          <button className="five" id="buttons">
+          <button
+            className="five"
+            id="buttons"
+            onClick={onClickSigns}
+            value="5"
+          >
             5
           </button>
-          <button className="zero" id="buttons">
+          <button
+            className="zero"
+            id="buttons"
+            onClick={onClickSigns}
+            value="0"
+          >
             0
           </button>
-          <button className="clear" id="buttons">
+          <button className="clear" id="buttons" onClick={clear}>
             AC
           </button>
-          <button className="six" id="buttons">
+          <button className="six" id="buttons" onClick={onClickSigns} value="6">
             6
           </button>
-          <button className="one" id="buttons">
+          <button className="one" id="buttons" onClick={onClickSigns} value="1">
             1
           </button>
-          <button className="two" id="buttons">
+          <button className="two" id="buttons" onClick={onClickSigns} value="2">
             2
           </button>
-          <button className="three" id="buttons">
+          <button
+            className="three"
+            id="buttons"
+            onClick={onClickSigns}
+            value="3"
+          >
             3
           </button>
-          <button className="decimal" id="buttons">
+          <button
+            className="decimal"
+            id="buttons"
+            onClick={onClickSigns}
+            value="."
+          >
             .
           </button>
-          <button className="equals" id="buttons">
+          <button className="equals" id="buttons" onClick={result}>
             =
           </button>
         </div>
